63d9aab93979f1186ae0f4c5288bc0a30bd681ab
baka-bon <193638775+baka-bon@users.noreply.github.com>
2025-12-02 00:23:26 +0700

    Update 16/11/2025

---
+++ fix_index_link.sh
@@ -0,0 +1,5 @@
+find . -type f -name "*.html" -mindepth 2 -maxdepth 2 -exec perl -0777 -i -pe 's/href="https:\/\/truyensextv55\.com"/href="index.html"/mg' {} \;
+find . -type f -name "*.html" -mindepth 3 -maxdepth 3 -exec perl -0777 -i -pe 's/href="https:\/\/truyensextv55\.com"/href="..\/index.html"/mg' {} \;
+find . -type f -name "*.html" -mindepth 4 -maxdepth 4 -exec perl -0777 -i -pe 's/href="https:\/\/truyensextv55\.com"/href="..\/..\/index.html"/mg' {} \;
+find . -type f -name "*.html" -mindepth 5 -maxdepth 5 -exec perl -0777 -i -pe 's/href="https:\/\/truyensextv55\.com"/href="..\/..\/..\/index.html"/mg' {} \;
+find . -type f -name "*.html" -mindepth 6 -maxdepth 6 -exec perl -0777 -i -pe 's/href="https:\/\/truyensextv55\.com"/href="..\/..\/..\/..\/index.html"/mg' {} \;
