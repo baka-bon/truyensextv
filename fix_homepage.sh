ad7d4208412bc3f3c7988fe9fb2c1a7adeb1cb27
baka-bon <193638775+baka-bon@users.noreply.github.com>
2025-12-02 00:23:30 +0700

    Update 17/11/2025

--- fix_homepage.sh
+++ fix_homepage.sh
@@ -3,3 +3,4 @@ find . -type f -name "*.html" -mindepth 3 -maxdepth 3 -exec perl -0777 -i -pe 's
 find . -type f -name "*.html" -mindepth 4 -maxdepth 4 -exec perl -0777 -i -pe 's/href="https:\/\/truyensextv55\.com"/href="..\/..\/index.html"/mg' {} \;
 find . -type f -name "*.html" -mindepth 5 -maxdepth 5 -exec perl -0777 -i -pe 's/href="https:\/\/truyensextv55\.com"/href="..\/..\/..\/index.html"/mg' {} \;
 find . -type f -name "*.html" -mindepth 6 -maxdepth 6 -exec perl -0777 -i -pe 's/href="https:\/\/truyensextv55\.com"/href="..\/..\/..\/..\/index.html"/mg' {} \;
+find . -type f -name "*.html" -exec perl -0777 -i -pe 's/" >/">/mg' {} \;
---
+++ git-add.sh
@@ -0,0 +1,4 @@
+git add truyensextv55.com/truyen-sex-dai-tap
+git add truyensextv55.com/truyen-sex-ngan
+git add truyensextv55.com/page
+git add truyensextv55.com/tag
