631dd94ddd1813a43f35d1ce6c34b6a3a28bc21c
baka-bon <193638775+baka-bon@users.noreply.github.com>
2025-12-11 01:21:13 +0700

    Update catalog 10/12/2025

--- git-ignore.sh
+++ git-ignore.sh
@@ -1,9 +1,2 @@
-# mark so Git ignores local edits
-#git update-index --skip-worktree truyensextv99.net/thu-ky-va-nguoi-tinh/
-
-# to undo
-#git update-index --no-skip-worktree path/to/file
-
-# alternative (less recommended)
+#!/bin/bash
 find truyensextv99.net/thu-ky-va-nguoi-tinh -type f -exec git update-index --assume-unchanged {} \;
-#git update-index --no-assume-unchanged path/to/file
---
+++ git-unignore.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+find truyensextv99.net/thu-ky-va-nguoi-tinh -type f -exec git update-index --no-assume-unchanged {} \;
