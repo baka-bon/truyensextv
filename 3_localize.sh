4b2c1c0168b30b94d5aff24459c35a22d4d8bfc2
baka-bon <193638775+baka-bon@users.noreply.github.com>
2026-01-05 07:07:26 +0700

    Update 04/01/2026

--- 3_localize.sh
+++ 3_localize.sh
@@ -6,3 +6,5 @@ find . -type f -name "*.html" -mindepth 3 -maxdepth 3 -exec dos2unix {} \; -exec
 find . -type f -name "*.html" -mindepth 4 -maxdepth 4 -exec dos2unix {} \; -exec perl -0777 -i -pe 's/href="https:\/\/truyensextv68\.com\/((?!amp\.html)[^"]+\/)?"/href="..\/..\/$1index.html"/mg' {} \;
 find . -type f -name "*.html" -mindepth 5 -maxdepth 5 -exec dos2unix {} \; -exec perl -0777 -i -pe 's/href="https:\/\/truyensextv68\.com\/((?!amp\.html)[^"]+\/)?"/href="..\/..\/..\/$1index.html"/mg' {} \;
 find . -type f -name "*.html" -mindepth 6 -maxdepth 6 -exec dos2unix {} \; -exec perl -0777 -i -pe 's/href="https:\/\/truyensextv68\.com\/((?!amp\.html)[^"]+\/)?"/href="..\/..\/..\/..\/$1index.html"/mg' {} \;
+
+git status
